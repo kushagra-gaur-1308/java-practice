d6728557024e49d57f2afac88f39d6b9f8a860e0
Kushagra Gaur <kushagraupes29@gmail.com>
2026-01-28 20:48:40 +0530

    Use of String

---
+++ Lab2.java
@@ -0,0 +1,8 @@
+public class Lab2{
+    static int[] arr = {1, 2, 3, 4, 2};
+    static int num = 10;
+    public static void main(String [] args){
+        System.out.println("Static variable num: " + num);
+        System.out.println("Array elements : " + arr[0]+ " " + arr[1]);
+    }
+}
