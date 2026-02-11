cf0b2bce69f0d38b5242a95727bc1aef4170a2c2
Kushagra Gaur <kushagraupes29@gmail.com>
2026-02-11 19:23:25 +0530

    Super keywords
    In java, super is only used once in the child class not more than that and same with this keyword.

--- ChainingSuper.java
+++ ChainingSuper.java
@@ -40,18 +40,3 @@ public class ChainingSuper {
         B obj = new B();
     }
 }
-
-class Add {
-    int sum(int a, int b) {
-        return a + b; }
-    int sum(int a, int b, int c) {
-        return a + b + c; }
-
-public class CompileTimePolymorphism {
-    public static void main(String[] args) {
-        Add obj = new Add();
-        System.out.println(obj.sum(2, 3));
-        System.out.println(obj.sum(1, 2, 3));
-        }
-    }
-}
